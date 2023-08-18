156bae01f2cad904ce83ba1dc6610c96e78ec58b
Walter Liendo <walter.liendo@gmail.com>
2023-08-17 22:13:36 -0300

    validacion de inputs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import FormSignUp from './Components/FormSignUp';
+import { Container } from '@mui/material';
+import Typography from '@mui/material/Typography';
 
 function App() {
+  const handleSubmit = ( valores ) => {
+    console.log('APPS:', valores);
+  }
+
   return (
-    <>
-    <h1>Formulario Registro</h1>
-      <FormSignUp />
-    </>
+    <Container container="section" maxWidth="sm">
+    <Typography variant='h3' align='center'>Formulario Registro</Typography>
+      <FormSignUp handleSubmit={handleSubmit} />
+    </Container>
   );
 }
 
--- src/Components/FormSignUp.jsx
+++ src/Components/FormSignUp.jsx
@@ -1,16 +1,52 @@
 import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
+import Switch from '@mui/material/Switch';
+import {TextField} from '@mui/material';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormGroup from '@mui/material/FormGroup';
+import { useState } from 'react';
+import FormControl from '@mui/material';
 
+function FormSignUp( props ) {
+    const {handleSubmit} = props
+    const [name, setName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [email, setEmail] =useState('')
+    const [prom, setProm] = useState(true)
+    const [nov, setNov] = useState(false)
 
-function FormSignUp() {
+    const [erros, setErros] = useState({
+        name: { 
+                error: false,
+                message: 'Deben ser al menos 3 caracteres'}
+    })
+
+    function validarNombre(nombre) {
+        if(nombre.length >= 3 ){
+            return { name: { error: false, message: ''}}
+        } else {
+            return { name: { error: true, message: 'Deben ser al menos 3 caracteres'}}
+        }
+    }
+ 
     return ( 
-    <form>
+    <form onSubmit={(e) =>{ e.preventDefault()
+                            handleSubmit({name,lastName,email,prom,nov})
+    }}>
         <TextField 
             id='name' 
             label="Nombre" 
             variant='outlined'
             fullWidth
             margin='normal'
+            onChange={(e) => { 
+                setName(e.target.value)
+            } }
+            value={name}
+            error={ erros.name.error}
+            helperText={ erros.name.error ? erros.name.message : ''}
+            onBlur={(e) => {
+                setErros(validarNombre(e.target.value))
+            }}
             />
         <TextField 
             id='lastName' 
@@ -18,6 +54,11 @@ function FormSignUp() {
             variant='outlined'
             fullWidth
             margin='normal'
+            value={lastName}
+            onChange={(e) => { 
+                setLastName(e.target.value)
+            } }
+
             />
         <TextField 
             id='email' 
@@ -25,14 +66,36 @@ function FormSignUp() {
             variant='outlined'
             fullWidth
             margin='normal'
+            value={email}
+            onChange={(e) => { 
+                setEmail(e.target.value)
+            } }
             />
         
-        <label>Promociones</label>
-        <input type="checkbox" />
-        <label>Novedades</label>
-        <input type="checkbox" />
-        <Button variant='contained'>Registrarse</Button>
-        
+        <FormGroup>
+            <FormControlLabel 
+                control={
+                <Switch 
+                    checked={prom}
+                    onChange={(e) => {
+                        setProm(e.target.checked)
+                    }}
+            />} 
+            label="Promociones"/>
+            <FormControlLabel 
+                control={
+                <Switch 
+                    checked={nov}
+                    onChange={(e) => {
+                        setNov(e.target.checked)
+                    }}
+            />} label="Novedades"/>
+        </FormGroup>
+        <Button 
+            variant='contained'
+            type='submit'>
+            Registrarse
+            </Button>
     </form>
     )
 }
